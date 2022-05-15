d45af62a5cce59104f411f8986838af0e8147ec1
Akshay1760 <101631328+Akshay1760@users.noreply.github.com>
2022-05-15 12:41:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 	public static void main(String[] args) {
 		
 			System.out.println("ASDM ");
-			System.out.println("Assignment 10 ")		
+			System.out.println("Assignment 10 ");		
 	}
 
 }
